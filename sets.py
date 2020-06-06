a5881bdc2c1dfa571da3b1467d8549b14769193e
Arif <arifkhan65@gmail.com>
2020-06-06 18:29:56 +0200

    Initial commit

---
+++ sets.py
@@ -0,0 +1,14 @@
+# Frozen set is immutable set
+list1=[1,2,3,4,1,2,3,10]
+x=set(list1)
+print('duplicates removed',x)
+s1=set([7,9,12,7,9])
+s2=set(['abc',12,'b','car',7,10,12])
+s3=set([12,14,12,'ab'])
+print(s1|s2)
+print(s1&s2)
+print('b'in s2)
+print('ab'in s2)
+print('ab'in s3)
+print(s2.discard(12))
+print(s1&s2)^s3
